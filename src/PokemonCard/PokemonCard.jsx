a0e94a9d71581e89baebe59e9e4047e6150322ca
yannisyannisyannis <yannis.kheyar@gmail.com>
2023-10-12 14:27:46 +0200

    deuxieme branche quête 4

--- src/PokemonCard/PokemonCard.jsx
+++ src/PokemonCard/PokemonCard.jsx
@@ -1,8 +1,38 @@
-function PokemonCard(){ 
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-        
-        <figcaption>Bulbasaur</figcaption>
-        </figure>
-}
+
+const pokemonList=[ 
+    { name:'Bulbasaur',
+    imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+
+{ 
+    name:'Mew',
+},
+];
+
+function PokemonCard() {
+
+    const pokemon = pokemonList[1];
+
+    if (pokemon.name && pokemon.imgSrc != null)
+    
+    {
+
+    return  ( <> 
+    <p> {pokemon.name}</p>
+    <img src= {pokemon.imgSrc}/>
+    </>
+    );
+    }
+     return(
+    <>
+
+    <p>???</p>
+    
+     </> 
+     
+     );
+
+     }
+
 
 export default PokemonCard
